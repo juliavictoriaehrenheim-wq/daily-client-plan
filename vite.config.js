87be1445cd22f9758723c90a4021a23ab0e07686
juliavictoriaehrenheim-wq <juliavictoria.ehrenheim@gmail.com>
2026-05-06 01:17:40 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 })
